11824e3bc824a3b3190de4f10be8ed742b793cbb
Conor Tompkins <conorotompkins@Conors-MBP.localdomain>
2022-07-09 13:12:13 -0400

    created script to get tract history

---
+++ scripts/prep/get_tract_history.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+library(tidycensus)
+library(sf)
+
+options(tigris_use_cache = TRUE)
+
+ac_geo <- c(2010, 2020) %>% 
+  map({~get_acs(geography = "tract", 
+               state = "Pennsylvania",
+               county = "Allegheny County",
+               variables = c(medincome = "B19013_001"),
+               year = .x,
+               geometry = T) %>% 
+      mutate(census_year = .x) %>% 
+      select(census_year, GEOID, geometry)},
+      .id = "census_year")
+  
+ac_geo %>% 
+  bind_rows() %>% 
+  st_transform(4326) %>% 
+  st_write("inputs/allegheny_county_tract_history/allegheny_county_tract_history.shp")
