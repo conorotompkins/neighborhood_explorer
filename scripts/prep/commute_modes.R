d630906a0226faef7f16cfcf1e5312a8c9494c15
Conor Tompkins <conorotompkins@Conors-MBP.localdomain>
2022-08-03 17:51:48 -0400

    switched to x_axis variable, reorderd variables

--- scripts/prep/commute_modes.R
+++ scripts/prep/commute_modes.R
@@ -7,6 +7,9 @@ options(tigris_use_cache = TRUE)
 acs1_vars <- load_variables(2019, 'acs1') %>% 
   mutate(across(c(label, concept), str_to_lower))
 
+acs1_vars %>% 
+  filter(str_detect(name, "B08301"))
+
 all_transit_vars <- c("B08301_003", 
                       "B08301_004", 
                       "B08301_010", 
@@ -17,10 +20,6 @@ all_transit_vars <- c("B08301_003",
                       "B08301_020",
                       "B08301_021")
 
-acs1_vars %>% 
-  filter(str_detect(name, "B08301")) %>% 
-  view()
-
 all_transit_modes <- get_acs(geography = "tract", 
                              variables = acs1_vars %>%
                                filter(name %in% all_transit_vars) %>%
@@ -28,8 +27,9 @@ all_transit_modes <- get_acs(geography = "tract",
                              summary_var = "B08301_001",
                              year = 2019, state = "PA", county = "Allegheny",
                              geometry = F) %>% 
-  select(GEOID, variable, estimate, moe, summary_est) %>% 
-  mutate(NAME = str_c("Tract", GEOID, sep = " "),
+  mutate(x_axis = "Commuters",
+         NAME = str_c("Tract", GEOID, sep = " "),
          graph_type = "discrete",
          census_year = 2010) %>% 
+  select(GEOID, NAME, x_axis, variable, estimate, moe, census_year, graph_type) %>% 
   write_csv("inputs/data_sources/commute_modes.csv")
