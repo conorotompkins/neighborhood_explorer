26fcdb1c7badbd4a924ddef20441a89dc105ed9e
Conor Tompkins <conorotompkins@Conors-MBP.localdomain>
2022-08-07 15:28:47 -0400

    switched to multiple year data source for median income

--- scripts/prep/median_income.R
+++ scripts/prep/median_income.R
@@ -4,15 +4,17 @@ library(sf)
 
 options(tigris_use_cache = TRUE)
 
-get_acs(geography = "tract", 
-                  state = "Pennsylvania",
-                  county = "Allegheny County",
-                  variables = c("Median Income" = "B19013_001"),
-                  year = 2020,
-                  geometry = T) %>% 
-  select(GEOID, geometry, variable, estimate, moe) %>% 
-  mutate(NAME = str_c("Tract", GEOID, sep = " "),
-         graph_type = "point_in_time",
-         census_year = 2020) %>% 
-  st_drop_geometry() %>% 
+c(2010:2019) %>% 
+  set_names() %>% 
+  map_dfr({~get_acs(geography = "tract", 
+                state = "Pennsylvania",
+                county = "Allegheny County",
+                variables = c("Median Income" = "B19013_001"),
+                year = .x,
+                geometry = F) %>%
+      mutate(NAME = str_c("Tract", GEOID, sep = " "),
+             graph_type = "point_in_time",
+             census_year = 2020)},
+      .id = "year") %>% 
+  select(GEOID, year, variable, estimate, moe) %>% 
   write_csv("inputs/data_sources/median_income.csv")
